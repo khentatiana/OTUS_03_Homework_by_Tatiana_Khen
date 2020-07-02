5e428649ccedfb12b1685e97c2d0b8364de51a56
tatiana <khen.tatiana@gmail.com>
2020-07-02 02:13:35 -0700

    Added YandexTest

--- ExplicitImplicitWaitProject/src/main/java/base/BaseTest.java
+++ ExplicitImplicitWaitProject/src/main/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import manager.WebDriverFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public abstract class BaseTest {
 
     protected static WebDriver driver;
+    protected static WebDriverWait wait;
     protected static Logger logger = LogManager.getLogger(BaseTest.class);
 
 
@@ -25,8 +27,10 @@ public abstract class BaseTest {
         logger.info("{} driver is created ", browserType);
         System.out.println("======New driver " + browserType + " is created======");
         if (driver != null) {
-            driver.manage().window().maximize();
+            wait = new WebDriverWait(driver, 30);
             driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+            driver.manage().window().maximize();
         }
     }
 
--- ExplicitImplicitWaitProject/src/test/java/tests/HomePageTest.java
+++ ExplicitImplicitWaitProject/src/test/java/tests/HomePageTest.java
@@ -1,11 +1,15 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class HomePageTest extends BaseTest {
 
-    String url = "https://otus.ru/";
+    String url = "https://yandex.ru";
     @Test
     public void openHomePage(){
         driver.get(url);
@@ -19,6 +23,12 @@ public class HomePageTest extends BaseTest {
         System.out.println("======" + title + "======");
         System.out.println();
         logger.info("Title of opened url {}", title );
+
+        WebElement input = driver.findElement(By.xpath("//input[@id='text']"));
+        input.sendKeys("otus");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable
+                (By.xpath("//div[@href='https://otus.ru/']"))).click();
 
     }
 }
